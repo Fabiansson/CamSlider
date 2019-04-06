ee02f9923fa3c5f073268359145a7fabd7246926
fabianzbinden@outlook.com <fabian@Silver.lan.loc>
2019-04-06 13:19:29 +0200

    wizard started

--- public/client.js
+++ public/client.js
@@ -1,5 +1,45 @@
+const route1 = ['mode', 'init', 'autonomous', 'parameters', 'anchors', 'overview'];
+
+var currentRoute = null;
+var currentStep = null;
+
+function renderContent(step) {
+    $('#app').load(step + '.html');
+}
+
+function setMode(mode) {
+    //setup.setMode(mode);
+
+    switch (mode) {
+        case 'timelapse':
+            currentRoute = route1;
+            break;
+        case 'pano':
+            currentRoute = route2;
+            break;
+        case 'movie':
+            currentRoute = route3;
+            break;
+        default:
+            break;
+    }
+    next();
+}
+
+function next() {
+    var nextStep = currentRoute.indexOf(currentStep) + 1
+
+    if(currentRoute[nextStep] === 'undefined'){
+        //TODO - SOCKET START WORK
+    }else{
+        renderContent(currentRoute[nextStep]);
+    }
+}
+
 $(document).ready(function () {
     var socket = io.connect('http://localhost:8000');
+
+    currentStep = 'mode';
 
     socket.on('news', function (data) {
         console.log(data);
@@ -8,30 +48,19 @@ $(document).ready(function () {
         });
     });
 
-    var button = document.getElementById('increaser');
-
-    $("input.spin").on("click", function(){
-       socket.emit('spin', {
-           my: 'button clicked'
-       }); 
+    $("input.spin").on("click", function () {
+        socket.emit('spin', {
+            my: 'button clicked'
+        });
     });
 
-    /*var iv;
-    $("input.increaser").on("mousedown touchstart", function () {
-
-        iv = setInterval(function () {
-        socket.emit('make step', {
-            interval: 0.0001});
-        }, 500);
-    }).on("mouseup mouseleave touchend", function () {
-        clearInterval(iv);
-    });*/
-
-    $('input.increaser').on('mousedown touchstart', function(){
+    $('input.increaser').on('mousedown touchstart', function () {
         socket.emit('start looping');
-    }).on('mouseup mouseleave touchend', function(){
+    }).on('mouseup mouseleave touchend', function () {
         socket.emit('stop looping');
     })
 
-    
+    renderContent(currentStep);
+
+
 });
