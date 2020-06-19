eaee718f662ac2427b78860ba8453faafaf60b16
Fabiansson <fabianzbinden@outlook.com>
2020-06-19 15:32:02 +0200

    also logging to console when in production

--- logger.js
+++ logger.js
@@ -18,7 +18,8 @@ const logger = winston.createLogger({
     ),
     transports: [
         new winston.transports.File({ filename: 'error.log', level: 'error' }),
-        new winston.transports.File({ filename: 'combined.log' })
+        new winston.transports.File({ filename: 'combined.log' }),
+        new winston.transports.Console({ format: winston.format.combine(winston.format.colorize(), winston.format.simple(), myFormat) })
     ]
 });
 
